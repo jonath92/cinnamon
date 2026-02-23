jonath92/cinnamon

commit c25b61d7f3a22eacc9ebb3bde1227ef5b4e3b4c4

st-texture-cache.c: Fix st_texture_cache_load_image_from_file_async.
This was applying the scale factor to images even though in many
places it's used this was already being done by its caller. This
resulted in inconsistencies depending on how the returned actor
was used.

Remove the internal scaling and fix callers that were unknowingly
relying on this behavior.

js/ui/keyboardManager.js
@@ -235,6 +235,7 @@ var SubscriptableFlagIcon = GObject.registerClass({
         this._subscript = null;
         this._file = null;
         this._image = null;
+        this._loadHandle = 0;
 
         super._init({
             style_class: 'input-source-switcher-flag-icon',
@@ -251,10 +252,10 @@ var SubscriptableFlagIcon = GObject.registerClass({
 
         this.add_child(this._drawingArea);
 
-        this.connect("allocation-changed", () => {
-            GLib.idle_add(GLib.PRIORITY_DEFAULT, () => {
+        this.connect('allocation-changed', () => {
+            if (this._image == null) {
                 this._load_file();
-            });
+            }
         });
     }
 
@@ -281,26 +282,22 @@ var SubscriptableFlagIcon = GObject.registerClass({
         }
 
         try {
-            St.TextureCache.get_default().load_image_from_file_async(
+            this._loadHandle = St.TextureCache.get_default().load_image_from_file_async(
                 this._file.get_path(),
                 -1, this.get_height(),
                 (cache, handle, actor) => {
-                    this._image = actor;
-                    let constraint = new Clutter.BindConstraint({
-                        source: actor,
-                        coordinate: Clutter.BindCoordinate.ALL
-                    })
+                    if (handle !== this._loadHandle) {
+                        return;
+                    }
 
-                    this._drawingArea.add_constraint(constraint);
+                    this._image = actor;
                     this._imageBin.set_child(actor);
+                    this._drawingArea.queue_repaint();
                 }
             );
-
         } catch (e) {
             global.logError(e);
         }
-
-        this._drawingArea.queue_relayout();
     }
 
     _drawingAreaRepaint(area) {
@@ -310,22 +307,13 @@ var SubscriptableFlagIcon = GObject.registerClass({
 
         const cr = area.get_context();
         const [w, h] = area.get_surface_size();
-        const surf_w = this._image.width;
-        const surf_h = this._image.height;
 
         cr.save();
 
-        // // Debugging...
-
-        // cr.setSourceRGBA(1.0, 1.0, 1.0, .2);
-        // cr.rectangle(0, 0, w, h);
-        // cr.fill();
-        // cr.save()
-
         if (this._subscript != null) {
-            let x = surf_w / 2;
+            let x = w / 2;
             let width = x;
-            let y = surf_h / 2;
+            let y = h / 2;
             let height = y;
             cr.setSourceRGBA(0.0, 0.0, 0.0, 0.5);
             cr.rectangle(x, y, width, height);
@@ -963,7 +951,7 @@ var InputSourceManager = class {
                 style_class: actorClass,
                 file: file,
                 subscript: source.dupeId > 0 ? String(source.dupeId) : null,
-                height: size,
+                height: size * global.ui_scale,
             });
         }
 

files/usr/share/cinnamon/applets/keyboard@cinnamon.org/applet.js
@@ -161,7 +161,7 @@ class CinnamonKeyboardApplet extends Applet.Applet {
             let actor = null;
 
             if (this._inputSourcesManager.showFlags) {
-                actor = this._inputSourcesManager.createFlagIcon(source, POPUP_MENU_ICON_STYLE_CLASS, 22 * global.ui_scale);
+                actor = this._inputSourcesManager.createFlagIcon(source, POPUP_MENU_ICON_STYLE_CLASS, 22);
             }
 
             if (actor == null) {

files/usr/share/cinnamon/applets/sound@cinnamon.org/applet.js
@@ -890,7 +890,8 @@ class Player extends PopupMenu.PopupMenuSection {
         }
         else {
             this._cover_path = cover_path;
-            this._cover_load_handle = St.TextureCache.get_default().load_image_from_file_async(cover_path, 300, 300, this._on_cover_loaded.bind(this));
+            const cover_size = 300 * global.ui_scale;
+            this._cover_load_handle = St.TextureCache.get_default().load_image_from_file_async(cover_path, cover_size, cover_size, this._on_cover_loaded.bind(this));
         }
     }
 
@@ -904,7 +905,7 @@ class Player extends PopupMenu.PopupMenuSection {
 
         // Make sure any oddly-shaped album art doesn't affect the height of the applet popup
         // (and move the player controls as a result).
-        actor.margin_bottom = 300 - actor.height;
+        actor.margin_bottom = (300 * global.ui_scale) - actor.height;
 
         this.cover = actor;
         this.coverBox.add_actor(this.cover);

files/usr/share/cinnamon/applets/xapp-status@cinnamon.org/applet.js
@@ -218,12 +218,13 @@ class XAppStatusIcon {
 
             // Assume symbolic icons would always be square/suitable for an StIcon.
             if (iconName.includes("/") && type != St.IconType.SYMBOLIC) {
+                const scaledIconSize = this.iconSize * global.ui_scale;
                 this.icon_loader_handle = St.TextureCache.get_default().load_image_from_file_async(
                     iconName,
                     /* If top/bottom panel, allow the image to expand horizontally,
                      * otherwise, restrict it to a square (but keep aspect ratio.) */
-                    this.actor.vertical ? this.iconSize : -1,
-                    this.iconSize,
+                    this.actor.vertical ? scaledIconSize : -1,
+                    scaledIconSize,
                     (...args)=>this._onImageLoaded(...args)
                 );
 

src/st/st-texture-cache.c
@@ -1690,7 +1690,6 @@ st_texture_cache_load_image_from_file_async (StTextureCache                  *ca
                                              StTextureCacheLoadImageCallback  callback,
                                              gpointer                         user_data)
 {
-  gint scale;
   if (callback == NULL)
     {
       g_warning ("st_texture_cache_load_image_from_file_async callback cannot be NULL");
@@ -1699,10 +1698,9 @@ st_texture_cache_load_image_from_file_async (StTextureCache                  *ca
 
   ImageFromFileAsyncData *data;
   GTask *result;
-  scale = st_theme_context_get_scale_for_stage (),
   data = g_new0 (ImageFromFileAsyncData, 1);
-  data->width = width == -1 ? -1 : width * scale;
-  data->height = height == -1 ? -1 : height * scale;
+  data->width = width;
+  data->height = height;
 
   static gint handles = 1;
   data->handle = handles++;

commit dd875efdb1114c7477554faa2bf7d0cb20574008

keyboardManager.js: Ensure the drawing area fills its parent size so the keyboard layout subscript can be drawn.
Regression from c25b61d7f3a22eacc9.

js/ui/keyboardManager.js
@@ -247,7 +247,10 @@ var SubscriptableFlagIcon = GObject.registerClass({
         this._imageBin = new St.Bin({ y_align: Clutter.ActorAlign.CENTER });
         this.add_child(this._imageBin);
 
-        this._drawingArea = new St.DrawingArea({});
+        this._drawingArea = new St.DrawingArea({
+            x_expand: true,
+            y_expand: true,
+        });
         this._drawingArea.connect('repaint', this._drawingAreaRepaint.bind(this));
 
         this.add_child(this._drawingArea);